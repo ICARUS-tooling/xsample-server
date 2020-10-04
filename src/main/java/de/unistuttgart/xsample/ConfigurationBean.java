43f835f5177868100d9e162c92fd309ae5d3b012
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-10-04 02:07:40 +0200

    Add some resources and do some widget experimentation.

--- src/main/java/de/unistuttgart/xsample/ConfigurationBean.java
+++ src/main/java/de/unistuttgart/xsample/ConfigurationBean.java
@@ -4,6 +4,8 @@
 package de.unistuttgart.xsample;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
 
 /**
  * @author Markus Gärtner
